a84d0286f5e1d0b01d96e2fee7d48b6157a6bbef
drewdulz <drewdulz@gmail.com>
2022-12-18 19:36:30 -0500

    setup main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,38 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+export default function Main() {
+  const [secondsRemaining, setSecondsRemaining] = useState(5)
 
-export default function Home() {
+  useEffect(() => {
+    const timeoutID = setTimeout(() => {
+      if (secondsRemaining > 0) {
+        setSecondsRemaining(secondsRemaining - 1)
+      } else {
+        window.location.href = 'https://www.sportsnet.ca'
+      }
+    }, 1000)
+
+    return () => {
+      clearTimeout(timeoutID)
+    }
+  }, [secondsRemaining])
+
   return (
-    <>
-      <Head>
-        <title>Not Sportsnet</title>
-        <meta name="description" content="This is not Sportsnet." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="text-3xl font-bold underline">Hello World!</main>
-    </>
+    <main>
+      <div className="relative px-6 lg:px-8 pt-20 sm:pt-32">
+        <h1 className="text-4xl text-center mb-4">
+          This is Sports<strong>ent</strong>.ca
+        </h1>
+        <div className="flex justify-center mx-auto ">
+          <div className="relative overflow-hidden rounded-full py-1.5 px-8 text-sm text-center text-gray-600 leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            You're probably looking for Sportsnet.ca.
+            <br />
+            <a href="https://www.sportsnet.ca" className="font-semibold text-indigo-600">
+              Taking you there in {secondsRemaining} <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
